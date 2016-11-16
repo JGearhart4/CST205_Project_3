88b315e02ad23b612ad8ef5a6d4096b33f042217
Kyle Butler-Fish <KyleOSX@Kyles-iMac.local>
2016-11-16 10:47:05 -0800

    Initial push again

--- example.py
+++ example.py
@@ -7,7 +7,14 @@ from dejavu.recognize import FileRecognizer, MicrophoneRecognizer
 
 # load config from a JSON file (or anything outputting a python dictionary)
 with open("dejavu.cnf.SAMPLE") as f:
-    config = json.load(f)
+    config = {
+		"database": {
+			"host": "127.0.0.1",
+			"user": "root",
+			"passwd": "teamProj",
+			"db": "dejavu",
+		}
+	}
 
 if __name__ == '__main__':
 
@@ -15,12 +22,13 @@ if __name__ == '__main__':
 	djv = Dejavu(config)
 
 	# Fingerprint all the mp3's in the directory we give it
-	djv.fingerprint_directory("mp3", [".mp3"])
+	djv.fingerprint_directory("Songs", [".mp3"])
 
 	# Recognize audio from a file
-	song = djv.recognize(FileRecognizer, "mp3/Sean-Fournier--Falling-For-You.mp3")
+	song = djv.recognize(FileRecognizer, "Songs/Bruno Mars - 24K Magic Lyrics.mp3")
 	print "From file we recognized: %s\n" % song
 
+"""
 	# Or recognize audio from your microphone for `secs` seconds
 	secs = 5
 	song = djv.recognize(MicrophoneRecognizer, seconds=secs)
@@ -33,3 +41,4 @@ if __name__ == '__main__':
 	recognizer = FileRecognizer(djv)
 	song = recognizer.recognize_file("mp3/Josh-Woodward--I-Want-To-Destroy-Something-Beautiful.mp3")
 	print "No shortcut, we recognized: %s\n" % song
+"""
