cbbe6d700aad606453f7a9b3473171c739779fa7
Kyle Butler-Fish <kbutler-fish@csumb.edu>
2016-12-07 11:43:16 -0800

    Finished recommendations

---
+++ recommendations.py
@@ -0,0 +1,300 @@
+def printRecommendations(song):
+	print "Reccomended songs:"
+
+	if (song["song_id"] == 1):
+		print "Stressed Out - Twenty One Pilots"
+		print "Closer - The Chainsmokers"
+	if (song["song_id"] == 2):
+		print "Mama Said - Lukas Graham"
+		print "See You Again - Wiz Khalifa ft. Charlie Puth"
+	if (song["song_id"] == 3):
+		print "Just Life Fire - P!nk"
+		print "Hello - Adele"
+	if (song["song_id"] == 4):
+		print "Halo - Beyonce"
+		print "If I Ain't Got You - Alicia Keys"
+	if (song["song_id"] == 5):
+		print "Dangerous Woman - Ariana Grande"
+		print "Side To Side - Ariana Grande ft. Ninki Minaj"
+	if (song["song_id"] == 6):
+		print "Into You - Ariana Grande"
+		print "Dangerous Woman - Ariana Grande"
+	if (song["song_id"] == 7):
+		print "My Humps - The Black Eyed Peas"
+		print "Fergalicious - Fergie"
+	if (song["song_id"] == 8):
+		print "Uptown Funk - Mark Ronson ft. Bruno Mars"
+		print "Ride - Twenty One Pilots"
+	if (song["song_id"] == 9):
+		print "Closer - The Chainsmokers"
+		print "Ride - Tweny One Pilots"
+	if (song["song_id"] == 10):
+		print "When I Was Your Man - Bruno Mars"
+		print "It Will Rain - Bruno Mars"
+	if (song["song_id"] == 11):
+		print "How Deep - Calvin Harris & Disciples"
+		print "Stitches - Shawn Mendes"
+	if (song["song_id"] == 12):
+		print "Broccoli - D.R.A.M ft. Lil Yachty"
+		print "Do You Mind - DJ Khaled ft. Nicki Minaj"
+	if (song["song_id"] == 13):
+		print "The Greatest - Sia"
+		print "Titanium - David Guetta ft. Sia"
+	if (song["song_id"] == 14):
+		print "Paradise - Coldplay"
+		print "Send My Love (To Your New Lover) - Adele"
+	if (song["song_id"] == 15):
+		print "Juju On That Beat - Zay Hilfigerrr & Zayion McCall"
+		print "Don't Mind - Kent Jones"
+	if (song["song_id"] == 16):
+		print "Scars To Your Beautiful - Alessia Cara"
+		print "Hide Away - Daya"
+	if (song["song_id"] == 17):
+		print "2 Phones - Kevin Gates"
+		print "Timmy Turner - Desiigner"
+	if (song["song_id"] == 18):
+		print "Broccoli - D.R.A.M"
+		print "Panda - Desiigner"
+	if (song["song_id"] == 19):
+		print "Side To Side - Ariana Grande ft. Nicki Minaj"
+		print "No Limit - Usher ft. Young Thug"
+	if (song["song_id"] == 20):
+		print "Hold You Down - DJ Khaled ft. Chris Brown"
+		print "Tru - Lloyd"
+	if (song["song_id"] == 21):
+		print "Let Me Love You - DJ Snake ft. Justin Bieber"
+		print "Lean On - DJ Snake & Major Lazer"
+	if (song["song_id"] == 22):
+		print "Cold Water - Major Lazer ft. Justin Bieber"
+		print "In The Name Of Love - Martin Garrix & Bebe Rexha"
+	if (song["song_id"] == 23):
+		print "Toothbrush - DNCE"
+		print "Body Moves - DNCE"
+	if (song["song_id"] == 24):
+		print "Cold Water - Major Lazer ft. Justin Bieber"
+		print "Side To Side - Ariana Grande ft. Nicki Minaj"
+	if (song["song_id"] == 25):
+		print "Scars To Your Beautiful - Alessia Cara"
+		print "Don't Wanna Know - Maroon 5"
+	if (song["song_id"] == 26):
+		print "One Dance - Drake"
+		print "Don't Mind - Kent Jones"
+	if (song["song_id"] == 27):
+		print "Lego House - Ed Sheeran"
+		print "Stitches - Shawn Mendes"
+	if (song["song_id"] == 28):
+		print "I'm Not The Only One - Sam Smith"
+		print "Cold Water - Major Lazer ft. Justin Bieber"
+	if (song["song_id"] == 29):
+		print "Don't Let Me Down - The Chainsmokers"
+		print "Lights - Ellie Goulding"
+	if (song["song_id"] == 30):
+		print "Going Under - Evanescence"
+		print "Sweet Sacrifice - Evanescence"
+	if (song["song_id"] == 31):
+		print "Hold Up - Beyonce"
+		print "Side To Side - Ariana Grande ft. Nicki Minaj"
+	if (song["song_id"] == 32):
+		print "Sorry - Justin Bieber"
+		print "Cold Water - Major Lazer ft. Justin Bieber"
+	if (song["song_id"] == 33):
+		print "BO$$ - Fifth Harmony"
+		print "Sorry - Justin Bieber"
+	if (song["song_id"] == 34):
+		print "Scars To Your Beautiful - Alessia Cara"
+		print "You've Got The Love - Florence And The Machine"
+	if (song["song_id"] == 35):
+		print "Scars To Your Beautiful - Alessia Cara"
+		print "Don't Wanna Know - Maroon 5"
+	if (song["song_id"] == 36):
+		print "Just Give Me A Reason - P!nk ft. Nate Ruess"
+		print "Royals - Lorde"
+	if (song["song_id"] == 37):
+		print "Cold Water - Major Lazer ft. Justin Bieber"
+		print "What They Want - Russ"
+	if (song["song_id"] == 38):
+		print "I Took A Pill In Ibiza - Mike Posner"
+		print "Scars To Your Beautiful - Alessia Cara"
+	if (song["song_id"] == 39):
+		print "What The Hell - Avril Lavigne"
+		print "Demons - Imagine Dragons"
+	if (song["song_id"] == 40):
+		print "Waiting On The World To Change - John Mayer"
+		print "Daughters - John Mayer"
+	if (song["song_id"] == 41):
+		print "Hotling Bling - Drake"
+		print "Uptown Funk - Mark Ronson ft. Bruno Mars"
+	if (song["song_id"] == 42):
+		print "This Is What You Came For - Calvin Harris"
+		print "Faded - Alan Walker"
+	if (song["song_id"] == 43):
+		print "Treat You Better - Shawn Mendes"
+		print "Don't Let Me Down - The Chainsmokers"
+	if (song["song_id"] == 44):
+		print "Counting Stars - OneRepublic"
+		print "Take Me To Church - Hozier"
+	if (song["song_id"] == 45):
+		print "Scars To Your Beautiful - Alessia Cara"
+		print "Side To Side - Ariana Grande ft. Nicki Minaj"
+	if (song["song_id"] == 46):
+		print "Scars To Your Beaufitul - Alessia Cara"
+		print "Side To Side - Ariana Grande ft. Nicki Minaj"
+	if (song["song_id"] == 47):
+		print "Locked Away - R City ft. Adam Levine"
+		print "Sorry - Justin Bieber"
+	if (song["song_id"] == 48):
+		print "Rock On - Tucker Beathard"
+		print "Backroad Song - Granger Smith"
+	if (song["song_id"] == 49):
+		print "Cold Water - Major Lazer ft. Justin Bieber"
+		print "Cake By The Ocean - DNCE"
+	if (song["song_id"] == 50):
+		print "Cry Me A River - Justin Timberlake"
+		print "Not A Bad Thing - Justin Timberlake"
+	if (song["song_id"] == 51):
+		print "Hot N Cold - Katy Perry"
+		print "Side To Side - Ariana Grande ft. Nicki Minaj"
+	if (song["song_id"] == 52):
+		print "Side To Side - Ariana Grande ft. Nicki Minaj"
+		print "Cold Water - Major Lazer ft. Justin Bieber"
+	if (song["song_id"] == 53):
+		print "I Took A Pill In Ibiza - Mike Posner"
+		print "Don't Wanna Know - Maroon 5"
+	if (song["song_id"] == 54):
+		print "The Greatest - Sia"
+		print "Million Reasons - Lady Gaga"
+	if (song["song_id"] == 55):
+		print "Bartender - Lady Antebellum"
+		print "What Hurts The Most - Rascal Flatts"
+	if (song["song_id"] == 56):
+		print "Starry Eyed - Ellie Goulding"
+		print "Anything Could - Ellie Goulding"
+	if (song["song_id"] == 57):
+		print "Criminal Mind - Lukas Graham"
+		print "Scars To Your Beautiful - Alessia Cara"
+	if (song["song_id"] == 58):
+		print "Let Me Love You - DJ Snake ft. Justin Bieber"
+		print "Scars To Your Beautiful - Alessia Cara"
+	if (song["song_id"] == 59):
+		print "24K Magic - Bruno Mars"
+		print "Blank Space - Taylor Swift"
+	if (song["song_id"] == 60):
+		print "Treat You Better - Shawn Mendes"
+		print "Don't Wanna Know - Maroon 5"
+	if (song["song_id"] == 61):
+		print "Marvin Gaye - Charlie Puth ft. Meghan Trainor"
+		print "Scars To Your Beautiful - Alessia Cara"
+	if (song["song_id"] == 62):
+		print "Blank Space - Taylor Swift"
+		print "All About That Bass - Meghan Trainor"
+	if (song["song_id"] == 63):
+		print "Summer - Calvin Harris"
+		print "Side To Side - Ariana Grande ft. Nicki Minaj"
+	if (song["song_id"] == 64):
+		print "Locked Away - R City ft. Adam Levine"
+		print "Scars To Your Beautiful - Alessia Cara"
+	if (song["song_id"] == 65):
+		print "Cold Water - Major Lazer ft. Justin Bieber"
+		print "Side To Side - Ariana Grande ft. Nicki Minaj"
+	if (song["song_id"] == 66):
+		print "So What - P!nk"
+		print "Love The Way You Lie - Eminem ft. Rihanna"
+	if (song["song_id"] == 67):
+		print "Scars To Your Beautiful - Alessia Cara"
+		print "Cold Water - Major Lazer ft. Justin Bieber"
+	if (song["song_id"] == 68):
+		print "Shake It Off - Taylor Swift"
+		print "Let Me Love You - DJ Snake ft. Justin Bieber"
+	if (song["song_id"] == 69):
+		print "Die Young - Ke$ha"
+		print "Counting Stars - OneRepublic"
+	if (song["song_id"] == 70):
+		print "Kiss It Better - Rihanna"
+		print "Pour It Up - Rihanna"
+	if (song["song_id"] == 71):
+		print "Love Song - Adele"
+		print "Diamonds - Rihanna"
+	if (song["song_id"] == 72):
+		print "Titanium - David Guetta"
+		print "Blank Space - Taylor Swift"
+	if (song["song_id"] == 73):
+		print "Kiss It Better - Rihanna"
+		print "One Dance - Drake"
+	if (song["song_id"] == 74):
+		print "Let Her Go - Passenger"
+		print "Counting Stars - One Republic"
+	if (song["song_id"] == 75):
+		print "Hello - Adele"
+		print "Skyfall - Adele"
+	if (song["song_id"] == 76):
+		print "Mercy - Shawn Mendes"
+		print "Into You - Ariana Grande"
+	if (song["song_id"] == 77):
+		print "Hotline Bling - Drake"
+		print "One Call Away - Charlie Puth"
+	if (song["song_id"] == 78):
+		print "Hymn For The Weekend - Coldplay"
+		print "One Dance - Drake"
+	if (song["song_id"] == 79):
+		print "Hips Don't Lie - Shakira"
+		print "Moves Like Jagger - Maroon 5"
+	if (song["song_id"] == 80):
+		print "The Greatest - Sia"
+		print "Sit Still, Look Pretty - Daya"
+	if (song["song_id"] == 81):
+		print "In The Name of Love - Martin Garrix"
+		print "Big Girls Cry - Sia"
+	if (song["song_id"] == 82):
+		print "Drive - Incubus"
+		print "Say It Ain't So - Weezer"
+	if (song["song_id"] == 83):
+		print "Purple Lamborghini - Skrillex"
+		print "Starboy - The Weeknd"
+	if (song["song_id"] == 84):
+		print "Thinking Out Loud - Ed Sheeran"
+		print "I'm Not the Only One - Sam Smith"
+	if (song["song_id"] == 85):
+		print "Black Beetles - Rae Sremmond ft. Gucci Mane"
+		print "Counting Stars - One Republic"
+	if (song["song_id"] == 86):
+		print "Heathens - Twenty One Pilots"
+		print "Radioactive - Imagine Dragons"
+	if (song["song_id"] == 87):
+		print "We Don't Talk Anymore - Charlie Puth"
+		print "This Is What You Came For Lyrics - Calvin Harris ft. Rihanna"
+	if (song["song_id"] == 88):
+		print "Take Me To Church - Hozier"
+		print "Mirrors - Justin Timberlake"
+	if (song["song_id"] == 89):
+		print "Timmy Turner - Desiigner"
+		print "Treat You Better - Shawn Mendes"
+	if (song["song_id"] == 90):
+		print "Just The Way You Are lyrics - Bruno Mars"
+		print "Thinking out Loud - Ed Sheeran"
+	if (song["song_id"] == 91):
+		print "Cheap Thrills - Sia ft. Sean Paul"
+		print "Starving - Hailee Steinfeld Grey feat Zedd"
+	if (song["song_id"] == 92):
+		print "Heathens- Twenty One Pilots"
+		print "Adele - Send My Love (To Your New Lover)"
+	if (song["song_id"] == 93):
+		print "Don't Let Me Down- The Chainsmokers ft. Daya"
+		print "This Is What You Came For - Calvin Harris ft. Rihanna"
+	if (song["song_id"] == 94):
+		print "Young M.A - OOOUUU"
+		print "Do You Mind - DJ Khaled ft Chris Brown Nicki Minaj August Alsina Jeremih Future Rick "
+	if (song["song_id"] == 95):
+		print "Meghan Trainor - Lips Are Movin'"
+		print "Gnash - I hate u, I love u"
+	if (song["song_id"] == 96):
+		print "Justin Bieber - What Do You Mean"
+		print "Shawn Mendes - Treat You Better"
+	if (song["song_id"] == 97):
+		print "Love Yourself - Justin Bieber"
+		print "Let me love you -DJ Snake _ Justin Bieber"
+	if (song["song_id"] == 98):
+		print "Ride - Twenty One Pilots"
+		print "Work from Home - Fifth Harmony ft. Ty Dolla $"
+	if (song["song_id"] == 99):
+		print "No Problem - Chance The Rapper ft. 2 Chainz and Lil Wayne"
+		print "Side To Side - Ariana Grande ft. Nicki Minaj"
